bbb63415a21af9c1d4620f999e39721d849ff526
Nabadeep <nabadeepdas069@gmail.com>
2025-03-04 16:18:20 +0530

    ini routes

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,4 +1,5 @@
-import React = require("react");
+"use client";
+import React from "react";
 
 export default function Signin() {
     return <div className="h-screen flex justify-center flex-col">
@@ -11,10 +12,11 @@ export default function Signin() {
                         </div>
                     </div>
                     <div className="pt-2">
-                        <LabelledInput label="Username" placeholder="nabadeep@gmail.com" />
+                        <LabelledInput label="Username" placeholder="yourmail@gmail.com" />
                         <LabelledInput label="Password" type={"password"} placeholder="123456" />
                         <button type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign up</button>
                     </div>
+                    <button type="button" onClick={() => window.location.href = './home'} className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Home</button>
                 </div>
             </a>
         </div>
