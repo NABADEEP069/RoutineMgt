094c57ddfe239839528eabcdaf10936d0f4c0456
Tushar Saha <sahatushar242002@gmail.com>
2025-03-04 21:57:26 +0530

    dept upd

--- app/dept/page.tsx
+++ app/dept/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from "react";
 
-export default function Signin() {
+export default function SelectSemester() {
     return <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
         <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
